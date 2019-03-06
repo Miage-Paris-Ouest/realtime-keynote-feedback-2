eb59f0d65620ffac1b2d61e7b168c93c349af4ca
Christelle <ilunga.christelle@ymail.com>
2019-03-06 10:55:59 +0100

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+
+
+./gradlew sonarqube \
+  -Dsonar.projectKey=Miage-Paris-Ouest_realtime-keynote-feedback-2 \
+  -Dsonar.organization=miage-paris-ouest-1 \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=af18b64fda8ac6afec703131abe46f517d41d5b4
