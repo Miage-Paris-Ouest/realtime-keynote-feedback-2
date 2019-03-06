6f94006282e5f58c02d4a036040d254300ddb043
Christelle <ilunga.christelle@ymail.com>
2019-03-06 11:09:52 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-
+#! /bin/bash
 
 ./gradlew sonarqube \
   -Dsonar.projectKey=Miage-Paris-Ouest_realtime-keynote-feedback-2 \
   -Dsonar.organization=miage-paris-ouest-1 \
   -Dsonar.host.url=https://sonarcloud.io \
-  -Dsonar.login=af18b64fda8ac6afec703131abe46f517d41d5b4
+  -Dsonar.login=4b0c0de06c2cb616e711dde7dc911512e4a6f6a6
