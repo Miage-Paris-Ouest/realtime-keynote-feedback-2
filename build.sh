f08c56ef429b776988f6581274f5a7879695f0ce
Christelle <ilunga.christelle@ymail.com>
2019-03-15 16:18:53 +0100

    Update build.sh
    Action de paramtrage - Changement de token

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
   -Dsonar.projectKey=Miage-Paris-Ouest_realtime-keynote-feedback-2 \
   -Dsonar.organization=miage-paris-ouest-1 \
   -Dsonar.host.url=https://sonarcloud.io \
-  -Dsonar.login=4b0c0de06c2cb616e711dde7dc911512e4a6f6a6
+  -Dsonar.login=4a2619a6b2108fb788121ad64b5c38018463b277
