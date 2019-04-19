eb65ca7bc780542643d179fdfb55a66a8be11703
Basile Mauvieux <bas.mauvieux@gmail.com>
2019-04-19 12:03:08 +0200

    build some basic functionns

--- src/main/java/miage/nanterre/m1app/realtimekeynote/controller/SeanceController.java
+++ src/main/java/miage/nanterre/m1app/realtimekeynote/controller/SeanceController.java
@@ -1,33 +1,33 @@
 package miage.nanterre.m1app.realtimekeynote.controller;
 
-import miage.nanterre.m1app.realtimekeynote.model.Seance;
-import miage.nanterre.m1app.realtimekeynote.model.SeanceAnalytics;
 import miage.nanterre.m1app.realtimekeynote.model.User;
-import miage.nanterre.m1app.realtimekeynote.repository.SeanceAnalyticsRepository;
 import miage.nanterre.m1app.realtimekeynote.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping(value = "/seance")
 public class SeanceController {
 
-    private SeanceAnalyticsRepository analyticsRepository;
     private UserRepository userRepository;
 
-    public SeanceController(SeanceAnalyticsRepository analyticsRepository, UserRepository userRepository) {
-        this.analyticsRepository = analyticsRepository;
+    public SeanceController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    @RequestMapping(value = "/all", method = RequestMethod.GET, headers = "Accept=application/json")
-    public List<Seance> getSeanceByUser(@RequestParam("userId") long userId) {
+    @RequestMapping(value = "/all/user", method = RequestMethod.GET, headers = "Accept=application/json")
+    public ResponseEntity<Object> getSeanceByUser(@RequestParam("userId") long userId) {
         User user = userRepository.findById(userId).get();
-        return user.getSeances();
+        return new ResponseEntity<Object>(user.getSeances(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET, headers = "Accept=application/json")
+    public ResponseEntity<Object> getAllSeance() {
+        return new ResponseEntity<Object>(userRepository.findAll(), HttpStatus.OK);
     }
 
 }
